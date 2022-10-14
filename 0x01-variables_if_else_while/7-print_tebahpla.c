bcbbf3849513f930c13b74f82c49cb3c9401272c
peculiar Emmanuel <ifechukwue03@gmail.com>
2022-10-14 12:30:34 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print the lowercase alphabrt in reverse.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a' ; alph = 'z' ; alph--)
+		putchar(alph);
+
+	putchar('\n');
+
+	return (0);
+}
