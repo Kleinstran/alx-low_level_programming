8eb9bb1fccf88c113305e8c9499a34dd9404bfe2
peculiar Emmanuel <ifechukwue03@gmail.com>
2022-10-18 13:25:38 -0700

    1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
-
 #include "main.h"
 
 /**
-  * main - This program prints the phrase _putchar
-  * Return: 0
+  * main - Prints "Holberton" followed by a new line.
+  *
+  * Return: Always 0.
   */
 int main(void)
 {
-	char ch[] = "_putchar";
-	int i;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	for (i = 0; ch[i] != '\0'; i++)
-	{
-		putchar(ch[i]);
-	}
-
-	putchar('\n');
 	return (0);
 }
