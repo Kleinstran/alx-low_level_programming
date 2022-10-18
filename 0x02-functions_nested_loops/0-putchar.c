791c8fb763a2c9a64309aa8290063391ad2a3796
peculiar Emmanuel <ifechukwue03@gmail.com>
2022-10-18 06:12:55 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,10 +3,19 @@
 /**
  * main - print _putchar
  *
- * Return: Always 0.
+ * Return 0.
  */
 int main(void)
 {
-	printf("_putchar");
+	char ch[]="_putchar";
+	int i;
+
+
+	for(i = 0; ch[i] != '\0'; i++)
+	{
+		_putchar(ch[i]);
+	}
+
+	_putchar('\n');
 	return (0);
 }
