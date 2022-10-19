1095e07b6ad7238aed08e1442d89361e7bcab116
peculiar Emmanuel <ifechukwue03@gmail.com>
2022-10-19 12:55:53 -0700

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+  *jack_bauer - prints every minute of the day
+  *Description: print every minute of the day
+  *Return: void
+  */
+void jack_bauer(void)
+{
+	int ht, ho, mt, mo, hm;
+
+	hm= 58;
+	ht = '0';
+	while (ht < '3')
+	{
+		if(ht == '2')
+		{
+			hm = '4';
+		}
+		ho = '0';
+		while (ho < hm)
+		{
+			mt = '0';
+			while (mt < '6')
+			{
+				mo = '0';
+				while (mo < 58)
+				{
+					_putchar(ht);
+					_putchar(ho);
+					_putchar(':');
+					_putchar(mt);
+					_putchar(mo);
+					_putchar('\n');
+					mo++;
+				}
+				mo = '0';
+				mt++;
+			}
+			mt = '0';
+			ho++;
+		}
+		ho = '0';
+		ht++;
+	}
+}
