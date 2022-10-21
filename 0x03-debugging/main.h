d808fedc812cd45ca45b0698ec5da85770af542f
peculiar Emmanuel <ifechukwue03@gmail.com>
2022-10-21 12:04:14 -0700

    3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,6 +2,8 @@
 #define main_h
 
 
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 int largest_number(int a, int b, int c);
 void positive_or_negative(int i);
 #endif
