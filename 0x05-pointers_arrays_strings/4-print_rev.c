b70c8f4c14f71738057ac5605b0be836da756e2e
peculiar Emmanuel <ifechukwue03@gmail.com>
2022-10-24 12:00:46 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * print_rev - print strings in reverse
+  *@s: input string.
+  *Return: no return
+  */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = '\0'; i != 0; i--)
+
+	return (i);
+}
