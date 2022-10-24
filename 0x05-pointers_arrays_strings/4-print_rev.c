2be22c2768e0e7c6f248fe54ebe6df61c8d7308e
peculiar Emmanuel <ifechukwue03@gmail.com>
2022-10-24 12:23:33 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,9 +7,13 @@
   */
 void print_rev(char *s)
 {
-	int i;
+	int i = '\0';
 
-	for (i = '\0'; i != 0; i--)
+	while (*(s + i) != 0)
+	{
+		i--;
+		_putchar(i);
+	}
 
-	return (i);
+	_putchar('\n');
 }
